3f37c7e1d72db9cd1f6762ad56e32c59ab2bc1b7
NikNitro <niknitrox@gmail.com>
2024-05-15 12:28:31 +0200

    Create content.js
    Created content.js for v1

---
+++ content.js
@@ -0,0 +1,46 @@
+
+var hostname = window.location.hostname;
+
+// Need 2 seconds for waiting to the blocker js to load
+setTimeout(function() {
+  if (hostname == 'elpais.com') {
+    theCountry();
+  } else if (hostname == 'www.elconfidencial.com') {
+    theConfidence();
+  } else if (hostname == 'www.marca.com') {
+    theConfidence();
+  } else if (hostname == 'www.diariosur.es') {
+    southDiary();
+  }
+
+}, 2000);
+
+
+var theCountry = function() {
+    // Find and remove the element with the ID "pmConsentWall" if it exists.
+    var wall = document.getElementById('pmConsentWall');
+    if (wall) {
+        wall.remove();
+    } 
+    // Change the style of the body's overflow to visible.
+    document.body.style.overflow = 'visible';
+}
+
+var theConfidence = function() {
+    // Find and remove the element with the ID 'didomi-popup' if it exists.
+    var wall = document.getElementById('didomi-popup');
+    if (wall) {
+        wall.remove();
+    } 
+   document.body.className = 'v2';
+}
+
+var southDiary = function() {
+    // Find and remove the element with the ID 'didomi-popup-open bodyBlocked' if it exists.
+    var wall = document.getElementById('engagement-top');
+    if (wall) {
+        wall.remove();
+    } 
+    // Change the style of the body's overflow to visible.
+    document.body.style.overflow = 'visible';
+}
