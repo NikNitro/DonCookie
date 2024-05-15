9ce6214b70fd9467c55fc000dd4f7b1b56ffb804
Migue27au <43569365+migue27au@users.noreply.github.com>
2024-05-15 13:33:18 +0200

    Añadido el theworld

--- content.js
+++ content.js
@@ -3,44 +3,55 @@ var hostname = window.location.hostname;
 
 // Need 2 seconds for waiting to the blocker js to load
 setTimeout(function() {
-  if (hostname == 'elpais.com') {
-    theCountry();
-  } else if (hostname == 'www.elconfidencial.com') {
-    theConfidence();
-  } else if (hostname == 'www.marca.com') {
-    theConfidence();
-  } else if (hostname == 'www.diariosur.es') {
-    southDiary();
-  }
+	if (hostname == 'elpais.com') {
+		theCountry();
+	} else if (hostname == 'www.elconfidencial.com') {
+		theConfidence();
+	} else if (hostname == 'www.marca.com') {
+		theConfidence();
+	} else if (hostname == 'www.diariosur.es') {
+		southDiary();
+	} else if (hostname == 'www.elmundo.es') {
+		theWorld();
+	}
 
 }, 2000);
 
 
 var theCountry = function() {
-    // Find and remove the element with the ID "pmConsentWall" if it exists.
-    var wall = document.getElementById('pmConsentWall');
-    if (wall) {
-        wall.remove();
-    } 
-    // Change the style of the body's overflow to visible.
-    document.body.style.overflow = 'visible';
+	 ument.body.style.overflow = 'visible';
 }
 
 var theConfidence = function() {
-    // Find and remove the element with the ID 'didomi-popup' if it exists.
-    var wall = document.getElementById('didomi-popup');
-    if (wall) {
-        wall.remove();
-    } 
-   document.body.className = 'v2';
+	// Find and remove the element with the ID 'didomi-popup' if it exists.
+	var wall = document.getElementById('didomi-popup');
+	if (wall) {
+			wall.remove();
+	} 
+	 document.body.className = 'v2';
 }
 
 var southDiary = function() {
-    // Find and remove the element with the ID 'didomi-popup-open bodyBlocked' if it exists.
-    var wall = document.getElementById('engagement-top');
-    if (wall) {
-        wall.remove();
-    } 
-    // Change the style of the body's overflow to visible.
-    document.body.style.overflow = 'visible';
+	// Find and remove the element with the ID 'didomi-popup-open bodyBlocked' if it exists.
+	var wall = document.getElementById('engagement-top');
+	if (wall) {
+			wall.remove();
+	} 
+	// Change the style of the body's overflow to visible.
+	document.body.style.overflow = 'visible';
+}
+
+/*
+ * author: migue27au
+ */
+var theWorld = function() {
+	// Find and remove the element with the ID 'didomi-popup-open bodyBlocked' if it exists.
+	console.log("Identified elmundo.es");
+	var wall = document.getElementById('didomi-popup');
+	console.log("DonCookie: " + wall);
+	if (wall) {
+			wall.remove();
+	} 
+	// Change the style of the body's overflow to visible.
+	document.body.className = 'v2';
 }
